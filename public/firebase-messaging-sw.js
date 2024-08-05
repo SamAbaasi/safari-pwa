50e5ffbd391c7529f49150d8d066c4e068cf7af1
hamed.par <hamed.par@mtnirancell.ir>
2024-08-05 20:32:30 +0330

    add

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -38,11 +38,22 @@ messaging.onBackgroundMessage(function(payload) {
       notificationOptions);
 });
 
-function handleClick (event) {
+self.addEventListener("notificationclick", (event) => {
+  console.log("On notification click: ", eevent.notification.data);
   event.notification.close();
-  // Open the url you set on notification.data\
-  console.log("event.notification.data", event.notification.data);
-  console.log("event.notification", event.notification);
-  clients.openWindow(event.notification.data.url)
-}
-self.addEventListener('notificationclick', handleClick);
+
+  // This looks to see if the current is already open and
+  // focuses if it is
+  event.waitUntil(
+    clients
+      .matchAll({
+        type: "window",
+      })
+      .then((clientList) => {
+        for (const client of clientList) {
+          if (client.url === "/" && "focus" in client) return client.focus();
+        }
+        if (clients.openWindow) return clients.openWindow("/about");
+      }),
+  );
+});
