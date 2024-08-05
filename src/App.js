a4a57874ba23b0075e2188a572448d546f6cce7e
hamed.par <hamed.par@mtnirancell.ir>
2024-08-05 19:35:43 +0330

    add router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import './App.css';
 import {ToastContainer, Zoom} from "react-toastify";
 import Notification from "./firebaseNotifications/Notification";
-import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Home View</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>About View</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
+    </div>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>404: Page Not Found</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
+    </div>
+  );
+}
 
 function App() {
-const [count, setCount] = useState(0)  
   return (
-    <div className="App">
+      <Router>
       <ToastContainer
           position="bottom-center"
           autoClose={5000}
@@ -21,9 +47,20 @@ const [count, setCount] = useState(0)
           closeButton={false}
       />
         <Notification/>
-        <h1>counter is: {count}</h1>
-        <button onClick={() => setCount(c => c + 1)}>Add counter</button>
-    </div>
+      <nav style={{ margin: 10 }}>
+        <Link to="/" style={{ padding: 5 }}>
+        Home
+        </Link>
+        <Link to="/about" style={{ padding: 5 }}>
+        About
+        </Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </Router>
   );
 }
 
