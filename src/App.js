39eaff24ec4099a4ac0995b3259e621871165b69
hamed.par <hamed.par@mtnirancell.ir>
2024-08-06 10:59:07 +0330

    add

--- src/App.js
+++ src/App.js
@@ -39,6 +39,7 @@ function App() {
 
     if("Notification" in window) {
       console.log(`"Notification" in window is true`);
+console.log("permisiion is: ",Notification.permission);
 
       if(Notification.permission !== "granted") {
         console.log(`Notification.permission !== "granted"`);
